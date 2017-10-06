f919247cfeb3a006688eed70ba223497d876c1c9
Jared Wolfe <wolfejar@ksu.edu>
2017-10-05 21:51:29 -0500

    Add project files.

---
+++ Rhyme Generator/UserInterface.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Rhyme_Generator
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxButton = new System.Windows.Forms.Button();
+            this.uxInput = new System.Windows.Forms.TextBox();
+            this.uxOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // uxButton
+            // 
+            this.uxButton.Location = new System.Drawing.Point(197, 12);
+            this.uxButton.Name = "uxButton";
+            this.uxButton.Size = new System.Drawing.Size(75, 52);
+            this.uxButton.TabIndex = 0;
+            this.uxButton.Text = "Generate Rhyme";
+            this.uxButton.UseVisualStyleBackColor = true;
+            this.uxButton.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxInput
+            // 
+            this.uxInput.Location = new System.Drawing.Point(12, 29);
+            this.uxInput.Name = "uxInput";
+            this.uxInput.Size = new System.Drawing.Size(179, 20);
+            this.uxInput.TabIndex = 1;
+            // 
+            // uxOutput
+            // 
+            this.uxOutput.Location = new System.Drawing.Point(12, 85);
+            this.uxOutput.Name = "uxOutput";
+            this.uxOutput.ReadOnly = true;
+            this.uxOutput.Size = new System.Drawing.Size(179, 20);
+            this.uxOutput.TabIndex = 2;
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 150);
+            this.Controls.Add(this.uxOutput);
+            this.Controls.Add(this.uxInput);
+            this.Controls.Add(this.uxButton);
+            this.Name = "UserInterface";
+            this.Text = "Rhyme Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxButton;
+        private System.Windows.Forms.TextBox uxInput;
+        private System.Windows.Forms.TextBox uxOutput;
+    }
+}
+
---
+++ Rhyme Generator/UserInterface.cs
@@ -0,0 +1,140 @@
+﻿/*UserInterface.cs
+ *Author: Jared Wolfe
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rhyme_Generator
+{
+    public partial class UserInterface : Form
+    {
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles button click event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxButton_Click(object sender, EventArgs e)
+        {
+            string input = uxInput.Text;
+            if (input.Length > 0)
+            {
+                uxOutput.Text = FindRhyme(input);
+            }
+            else
+            {
+                uxOutput.Text = "";
+            }
+        }
+
+        /// <summary>
+        /// Finds a rhyme to the given input.
+        /// </summary>
+        /// <param name="input">A string to find a rhyme for.</param>
+        /// <returns>A string rhyme.</returns>
+        private string FindRhyme(string input)
+        {
+            string output = input.ToLower().Trim();
+            string end = findEnding(output);
+            if (end == null)
+            {
+                return "";
+            }
+            string[] words = File.ReadAllLines(@"C: \Users\bigba_000\Documents\dictionary.txt");
+            StringBuilder sb = new StringBuilder();
+            int count = 0;
+            string vowels = "aeiouy";
+            foreach (string s in words)
+            {
+                if (s.Length >= end.Length)
+                {
+                    sb.Clear();
+                    sb.Append(s, s.Length - end.Length, end.Length);
+                    if (sb.ToString() == end)
+                    {
+                        count++;
+                    }
+                }
+            }
+            string[] rhymes = new string[count];
+            int secondCount = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length >= end.Length)
+                {
+                    sb.Clear();
+                    sb.Append(words[i], words[i].Length - end.Length, end.Length);
+                    if (sb.ToString() == end)
+                    {
+                        rhymes[secondCount] = words[i];
+                        secondCount++;
+                    }
+                }
+            }
+            if (rhymes.Length > 0)
+            {
+                Random rnd = new Random();
+                return rhymes[rnd.Next(rhymes.Length)];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Searches for the vowel in the string that creates the rhyme.
+        /// </summary>
+        /// <param name="s">The word to analyze.</param>
+        /// <returns>End of the word that creates the rhyme.</returns>
+        private string findEnding(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            string vowels = "aeiouy";
+            bool onlyVowel = false;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (vowels.Contains(s[i]))
+                {
+                    if (i != 0)
+                    {
+                        if (vowels.Contains(s[i - 1])) // if there are two vowels side by side, start with the first one.
+                        {
+                            i--;
+                        }
+                    }
+                    if (!(i == s.Length - 1))
+                    {
+                        onlyVowel = false;
+                        for (int k = i; k < s.Length; k++)
+                        {
+                            sb.Append(s[k]);
+                        }
+                        return sb.ToString();
+                    }
+                    else
+                    {
+                        onlyVowel = true;
+                    }
+                }
+            }
+            if (onlyVowel)
+            {
+                return s;
+            }
+            return null;
+        }
+    }
+}
